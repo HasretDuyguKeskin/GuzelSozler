52bb1acdceb81cc0ecb5653a83ece9219e262f5a
hasret keskin <hasret_1989_@hotmail.com>
2021-04-20 11:18:04 +0300

    Add project files.

---
+++ GuzelSozler/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GuzelSozler.Areas.Admin.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuzelSozler.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Kullanici>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<GuzelSoz> GuzelSozler { get; set; }
+    }
+}
---
+++ GuzelSozler/Data/GuzelSoz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuzelSozler.Areas.Admin.Data
+{
+    public class GuzelSoz
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Soz { get; set; }
+    }
+}
